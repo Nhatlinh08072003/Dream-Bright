c433c42c65908f1833586361d4822be5cc489b99
Nhatlinh08072003 <letiennhatlinh08072003@gmail.com>
2024-09-21 11:06:51 +0700

    faaga

--- obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1a27c31b29052026853a5fff283d383ce60aa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeafd84c5e5a8f0156898eea86ff234f44eb67e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
