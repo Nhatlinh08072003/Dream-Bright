4de401536cd947904a002681260100d8e2da12af
Hulenn <luuhonghuu530@gmail.com>
2024-10-25 20:52:21 +0700

    obj bin

--- obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf62319538159acaf6c5ecfff71523f18f9cb0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b14da69e46fc431fb57c93655c66b1d39be58fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
