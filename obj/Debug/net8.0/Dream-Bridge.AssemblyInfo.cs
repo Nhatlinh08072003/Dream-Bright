89de685e0b14ed68cea8d07c2e43dc4e6edec926
Nhatlinh08072003 <letiennhatlinh08072003@gmail.com>
2024-11-13 17:01:04 +0700

    nofitication

--- obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4ff1de925f061e429a588669e34b8ee6799501b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8688106c046960a6d113c798b5efb98152fd15ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
