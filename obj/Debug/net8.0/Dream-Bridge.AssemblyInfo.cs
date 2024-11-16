b7d6672207e390c486b2062c4ebeb6137f0d996c
wuan204 <144576545+wuan204@users.noreply.github.com>
2024-11-16 13:00:44 +0700

    fix

--- obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c44be36553c357ec233c5b283793c5f9a320d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aab97c834c335519ced9603e0f1a51547eeaab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
