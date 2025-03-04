65e7bd9a4c00f257013b5ec98705d6fff81fb4b7
Hulenn <luuhonghuu530@gmail.com>
2025-03-04 19:47:25 +0700

    dsada

--- obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dream-Bridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f898294d150d022283dcf0cdddd7bd0223721cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2badf0336144073de0760f7f08f7dc84c48d83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dream-Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
